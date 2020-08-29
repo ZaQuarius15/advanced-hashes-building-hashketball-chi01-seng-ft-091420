e8a1442045c780814e6a41a27c86fca4e5c43b70
Zack Carlson <jzcarlson014@gmail.com>
2020-08-29 19:17:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,36 @@
 def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black", "White"]
+      :players => [
+        {:player_name => ""
+        :number => 
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+        ]
+    }
+    :away => {
+      :team_name => ""
+      :colors => []
+      :players => [
+        :player_name => ""
+        :number => 
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+        ]
+    }
+  }
   
-  
-  
+  game_hash
 end 
